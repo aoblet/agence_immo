a71333db8436a93da715c7408cb69f5e6a3e86db
System Administrator <root@MacBook-Pro-de-Armand.local>
2014-04-20 21:09:06 +0200

    maj site

--- js/main.js
+++ js/main.js
@@ -1,26 +1,4 @@
-animheader = function(){
 
-	$(window).scroll(function(){
-		var $this = $(this),
-		pos   = $this.scrollTop();
-
-
-		if (pos > 300){
-			$('header').addClass('menu-small');
-			$('.first-section').addClass('pad-top');
-
-		} else {
-			$('header').removeClass('menu-small');
-			$('.first-section').removeClass('pad-top');
-		}
-	});
-};
-
-
-
-$(document).ready(function() {
-	animheader();
-});
 
 
 
