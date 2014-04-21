396ecd29a63607a3cf42db9f5272af578a26347f
System Administrator <root@mbp-de-armand.home>
2014-04-21 23:48:17 +0200

    grosse maj de sa mère

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
+$(function() {
+
+	// $(".fa-deconnect").hover(function(){
+		
+	// 	$(".deco-moove").addClass( "fa-unlock");
+	// 	// $(".deco-moove").removeClass("fa-lock");
+	// }else{
+	// 	$(".deco-moove").removeClass( "fa-unlock");
+	// });
 
 
+	$('.fa-deconnect').hover(
+       function(){ $(".deco-moove").addClass("fa-lock");$(".deco-moove").removeClass("fa-unlock") },
+       function(){ $(".deco-moove").removeClass("fa-lock");$(".deco-moove").addClass("fa-unlock") }
+)
 
-
+});
 
