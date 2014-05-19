a42b3f1885223bb9d7d4dedab1f6ca1abc3bef68
Armand <armandbiteau@gmail.com>
2014-05-20 00:46:12 +0200

    new messages html barre top

--- js/main.js
+++ js/main.js
@@ -12,7 +12,23 @@ $(function() {
 	$('.fa-deconnect').hover(
        function(){ $(".deco-moove").addClass("fa-lock");$(".deco-moove").removeClass("fa-unlock") },
        function(){ $(".deco-moove").removeClass("fa-lock");$(".deco-moove").addClass("fa-unlock") }
-)
+	)
+
+	var bool =0;
+	$( ".fa-message" ).click(function() {
+		if (bool == 0) {
+		  	$( "#new-messages" ).css("display","block");
+		  	bool =1;
+	  	}else{
+	  		$( "#new-messages" ).css("display","none");
+		  	bool =0;
+	  	}
+	});
+
+
+
+
+
 
 });
 
