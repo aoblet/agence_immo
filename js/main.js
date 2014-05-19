4fcf1879450061b226eec6eb7861c3fe1bb88be9
Armand <armandbiteau@gmail.com>
2014-05-20 01:37:15 +0200

    modal effect notifs

--- js/main.js
+++ js/main.js
@@ -18,17 +18,33 @@ $(function() {
 	$( ".fa-message" ).click(function() {
 		if (bool == 0) {
 		  	$( "#new-messages" ).css("display","block");
+		  	$( "#new-messages-modal" ).css("display","block");
 		  	bool =1;
 	  	}else{
 	  		$( "#new-messages" ).css("display","none");
+	  		$( "#new-messages-modal" ).css("display","none");
 		  	bool =0;
 	  	}
+	});
+
+	$( "#new-messages-modal" ).click(function() {
+	  		$( "#new-messages" ).css("display","none");
+	  		$( "#new-messages-modal" ).css("display","none");
+		  	bool =0;
 	});
 
 
 
 
+// 	$("body:not('#new-messages')").click(function(){
+// 		$("#new-messages").css("display","none");
+// 		bool =0;
+	
+// });
 
 
-});
 
+
+
+
+    });
